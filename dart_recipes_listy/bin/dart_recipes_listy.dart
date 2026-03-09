687f59621395ad2d8741a8721576be4dfd597342
MVNSouza <marcos.nasouza2@gmail.com>
2026-03-09 18:51:39 -0300

    first commit

---
+++ dart_recipes_listy/bin/dart_recipes_listy.dart
@@ -0,0 +1,5 @@
+import 'package:dart_recipes_listy/dart_recipes_listy.dart' as dart_recipes_listy;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_recipes_listy.calculate()}!');
+}
---
+++ dart_recipes_listy/test/dart_recipes_listy_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_recipes_listy/dart_recipes_listy.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
