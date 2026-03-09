02117e2d67d6e0849754b654a3496a9d61d452ac
MVNSouza <marcos.nasouza2@gmail.com>
2026-03-09 18:58:42 -0300

    feat: adicionado função `requestData` para puxar lista de pessoas de Gist

--- dart_assyncronism/bin/main.dart
+++ dart_assyncronism/bin/main.dart
@@ -2,6 +2,15 @@ import 'package:http/http.dart';
 
 void main() {
   print('Hello, World!');
+  requestData();
 }
 
-
+void requestData() {
+  String url =
+      "https://gist.githubusercontent.com/MVNSouza/6bfdfa496b5dae5a395c318ae9a4189d/raw/803ff5bcdf8e57f2effcc26f91d4dbc227bf6cc0/gistfile1.txt";
+  Future<Response> futureResponse = get(Uri.parse(url));
+  futureResponse.then((value) {
+    print(value);
+    print(value.body);
+  });
+}
