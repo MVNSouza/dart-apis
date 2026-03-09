f2ea5ba89ff018a826d05d513507019e26e7ab25
MVNSouza <marcos.nasouza2@gmail.com>
2026-03-09 19:06:37 -0300

    chore: correção do nome do diretório e arquivos relacionados

--- dart_recipes_listy/bin/dart_recipes_listy.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_recipes_listy/dart_recipes_listy.dart' as dart_recipes_listy;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_recipes_listy.calculate()}!');
-}
