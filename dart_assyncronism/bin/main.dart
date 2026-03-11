a32ee53f30f0c5e2acb7f72d46d09ce41e4a421d
MVNSouza <marcos.nasouza2@gmail.com>
2026-03-11 19:14:39 -0300

    feat: aperfeiçoando as saídas dos métodos utilizando Streams

--- dart_assyncronism/bin/main.dart
+++ dart_assyncronism/bin/main.dart
@@ -1,16 +1,24 @@
+import 'dart:async';
 import 'package:http/http.dart';
 import 'dart:convert';
 import '../lib/api_key.dart';
 
+StreamController<String> streamController = StreamController<String>();
 void main() {
-  print('Hello, World!');
+  StreamSubscription streamSubscription = streamController.stream.listen((
+    String info,
+  ) {
+    print(info);
+  });
+  requestData();
+  requestDataAsync();
   sendDataAsync({
     "id": "NEW001",
     "name": "Marcos",
     "lastName": "Souza",
     "balance": 350,
   });
-  //requestLivros();
+  requestLivros();
 }
 
 void requestData() {
@@ -18,25 +26,17 @@ void requestData() {
       "https://gist.githubusercontent.com/MVNSouza/6bfdfa496b5dae5a395c318ae9a4189d/raw/243aacc930bc646dfb8d597eb18145bedfe886e4/account.json";
   Future<Response> futureResponse = get(Uri.parse(url));
   futureResponse.then((Response response) {
-    print(response);
-    print(response.body);
-
-    List<dynamic> listRecipes = json.decode(response.body);
-    Map<String, dynamic> mapCarla = listRecipes.firstWhere(
-      (element) => element["name"] == "Carla",
+    streamController.add(
+      "${DateTime.now()} | Requisição de leitura (usando then)",
     );
-    print(mapCarla["balance"]);
   });
-
-  print(
-    'Última coisa a acontecer',
-  ); //Não é a última coisa a acontecer em tempo de execução
 }
 
 Future<List<dynamic>> requestDataAsync() async {
   String url =
       "https://gist.githubusercontent.com/MVNSouza/6bfdfa496b5dae5a395c318ae9a4189d/raw/243aacc930bc646dfb8d597eb18145bedfe886e4/account.json";
   Response response = await get(Uri.parse(url));
+  streamController.add("${DateTime.now()} | Requisição de leitura");
   return json.decode(response.body);
 }
 
@@ -45,13 +45,7 @@ void requestLivros() async {
       'https://raw.githubusercontent.com/alura-cursos/dart_assincronismo_api/aula05/.json/books.json';
 
   Response response = await get(Uri.parse(url));
-  List<dynamic> listResponse = json.decode(response.body);
-  for (dynamic livro in listResponse) {
-    Map<String, dynamic> atual = livro as Map<String, dynamic>;
-    if (atual["author"] == "Jorge Amado") {
-      print(atual["title"]);
-    }
-  }
+  streamController.add("${DateTime.now()} | Requisição de leitura de livros");
 }
 
 void sendDataAsync(Map<String, dynamic> mapAccont) async {
@@ -71,5 +65,14 @@ void sendDataAsync(Map<String, dynamic> mapAccont) async {
       },
     }),
   );
-  print(response.statusCode);
+
+  if (response.statusCode.toString()[0] == "2") {
+    streamController.add(
+      "${DateTime.now()} | Requisição de adição bem sucedida (${mapAccont["name"]})",
+    );
+  } else {
+    streamController.add(
+      "${DateTime.now()} | Requisição de adição falhou (${mapAccont["name"]})",
+    );
+  }
 }
